a033773f0a2cbe8e894d0a15d21bb8020fb5ecfc
jeohenry <efuoyoma@gmail.com>
2025-08-28 15:57:41 +0100

    Update app.jsx

--- src/app.jsx
+++ src/app.jsx
@@ -1,21 +1,5 @@
-import React, { Suspense, lazy } from "react";
-import { Routes, Route } from "react-router-dom";
-import Loading from "./components/Loading";
-import PageWrapper from "./components/PageWrapper";
-import Home from "./pages/Home";
-
-// Example lazy-loaded AI module
-// const ChatAI = lazy(() => import("./modules/Chat/ChatModule"));
-
 function App() {
-  return (
-    <Suspense fallback={<Loading />}>
-      <Routes>
-        <Route path="/" element={<PageWrapper><Home /></PageWrapper>} />
-        {/* <Route path="/chat" element={<PageWrapper><ChatAI /></PageWrapper>} /> */}
-      </Routes>
-    </Suspense>
-  );
+  return <h1>Hello World</h1>;
 }
 
 export default App;
