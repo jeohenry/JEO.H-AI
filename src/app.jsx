77a870367b3558955071f73d6dc1e433f61bc2eb
jeohenry <efuoyoma@gmail.com>
2025-08-28 15:31:55 +0100

    Update app.jsx

--- src/app.jsx
+++ src/app.jsx
@@ -1,20 +1,12 @@
-import React, { Suspense, lazy } from "react";
+import React from "react";
 import { Routes, Route } from "react-router-dom";
-import { AnimatePresence } from "framer-motion";
-
 import Home from "./pages/Home";
-import Loading from "./components/Loading";
-import PageWrapper from "./components/PageWrapper";
 
 function App() {
   return (
-    <AnimatePresence mode="wait">
-      <Suspense fallback={<Loading />}>
-        <Routes>
-          <Route path="/" element={<PageWrapper><Home /></PageWrapper>} />
-        </Routes>
-      </Suspense>
-    </AnimatePresence>
+    <Routes>
+      <Route path="/" element={<Home />} />
+    </Routes>
   );
 }
 
