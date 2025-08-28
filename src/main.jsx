89228e17f79d30aed9e58d4225a02596180d06b5
jeohenry <efuoyoma@gmail.com>
2025-08-28 16:00:58 +0100

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,8 @@
+// src/main.jsx
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";  // make sure this path is correct
-import './index.css';     // Tailwind CSS
+import App from "./App";
+import './index.css';
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
