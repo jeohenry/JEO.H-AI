f6b01e146518197a90ffc0024825edd73657281a
jeohenry <efuoyoma@gmail.com>
2025-08-29 07:32:51 +0100

    Update pagewrapper.jsx

--- src/components/pagewrapper.jsx
+++ src/components/pagewrapper.jsx
@@ -1,9 +1,19 @@
+// src/components/PageWrapper.jsx
 import React from "react";
+import { motion } from "framer-motion";
+import { defaultPageTransition } from "../config/animations";
 
-export default function PageWrapper({ children }) {
-  return (
-    <div className="p-4">
-      {children}
-    </div>
-  );
-}
+const PageWrapper = ({ children, animation = defaultPageTransition }) => (
+  <motion.div
+    initial={animation.initial}
+    animate={animation.animate}
+    exit={animation.exit}
+    transition={animation.transition}
+  >
+    {children}
+  </motion.div>
+);
+
+export default PageWrapper;
+
+
