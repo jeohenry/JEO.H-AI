57d39397290f6da83034b61a41ca82049749802f
jeohenry <efuoyoma@gmail.com>
2025-09-03 21:19:17 +0100

    Update ImageClassifier.tsx

--- src/modules/ImageClassifier.tsx
+++ src/modules/ImageClassifier.tsx
@@ -1,3 +1,5 @@
+// src/modules/ImageClassifier.tsx
+
 import React, { useState } from "react";
 import { Card, CardContent } from "@/components/ui/card";
 import { Button } from "@/components/ui/button";
