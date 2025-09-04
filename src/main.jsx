2897c878f01814de511f16a57bc0bfa08c051491
jeohenry <efuoyoma@gmail.com>
2025-09-04 20:14:35 +0100

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,21 +1,27 @@
+//src/main.jsx
+
 import React, { Suspense } from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { I18nextProvider } from "react-i18next";
 import i18n from "./i18n";
-import App from "./app.jsx";
+import App from "./App";
 import "./index.css";
 import { ThemeProvider } from "./context/themecontext.jsx";
+import { AuthProvider } from "@/context/AuthContext";
+import Loading from "@/components/Loading"; // ✅ new import
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <I18nextProvider i18n={i18n}>
       <ThemeProvider>
-        <BrowserRouter>
-          <Suspense fallback={<div>Loading...</div>}>
-            <App />
-          </Suspense>
-        </BrowserRouter>
+        <AuthProvider>
+          <BrowserRouter>
+            <Suspense fallback={<Loading message="Please wait..." />}>
+              <App />
+            </Suspense>
+          </BrowserRouter>
+        </AuthProvider>
       </ThemeProvider>
     </I18nextProvider>
   </React.StrictMode>
