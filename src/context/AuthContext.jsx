63d8e4a912ac604aca17740aa83e5da8e5815223
jeohenry <efuoyoma@gmail.com>
2025-09-04 20:06:25 +0100

    Create AuthContext.jsx

---
+++ src/context/AuthContext.jsx
@@ -0,0 +1,44 @@
+//src/context/AuthContext.jsx
+
+import React, { createContext, useEffect, useState } from "react";
+import axios from "@/api";
+
+export const AuthContext = createContext();
+
+export const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+    const userId = localStorage.getItem("userId");
+
+    if (!token || !userId) {
+      setUser(null);
+      setLoading(false);
+      return;
+    }
+
+    axios
+      .get(`/relationship/user/${userId}`, {
+        headers: { Authorization: `Bearer ${token}` },
+      })
+      .then((res) => {
+        setUser({ ...res.data, token }); // store role + token
+      })
+      .catch(() => {
+        setUser(null);
+        localStorage.removeItem("token");
+        localStorage.removeItem("userId");
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  }, []);
+
+  return (
+    <AuthContext.Provider value={{ user, loading, setUser }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
