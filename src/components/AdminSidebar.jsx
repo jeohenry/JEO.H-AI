c678239614b7fa2c6ee596775feff1dc483e96c0
jeohenry <efuoyoma@gmail.com>
2025-09-04 06:31:51 +0100

    Update AdminSidebar.jsx

--- src/components/AdminSidebar.jsx
+++ src/components/AdminSidebar.jsx
@@ -1,4 +1,4 @@
-// src/components/Sidebar/AdminSidebar.jsx
+// src/components/AdminSidebar.jsx
 
 import React, { useState, useEffect } from "react";
 import { NavLink, useNavigate, useLocation } from "react-router-dom";
