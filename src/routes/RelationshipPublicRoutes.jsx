6eff42c334de073bab926c83237db52c640ba7df
jeohenry <efuoyoma@gmail.com>
2025-09-04 19:52:09 +0100

    Update RelationshipPublicRoutes.jsx

--- src/routes/RelationshipPublicRoutes.jsx
+++ src/routes/RelationshipPublicRoutes.jsx
@@ -5,50 +5,18 @@ import { Route } from "react-router-dom";
 import PageWrapper from "@/components/PageWrapper";
 import { slideRight, scaleFade } from "@/config/animations";
 
-// Auth pages
 import Login from "@/pages/Auth/Login";
 import Register from "@/pages/Auth/Register";
 import PasswordReset from "@/pages/Auth/PasswordReset";
 import LanguageSelector from "@/pages/Auth/LanguageSelector";
 
-const PublicRoutes = () => (
+const RelationshipPublicRoutes = () => (
   <>
-    {/* Language selection entry point */}
-    <Route
-      path="language"
-      element={
-        <PageWrapper animation={scaleFade}>
-          <LanguageSelector />
-        </PageWrapper>
-      }
-    />
-
-    {/* Authentication routes */}
-    <Route
-      path="login"
-      element={
-        <PageWrapper animation={slideRight}>
-          <Login />
-        </PageWrapper>
-      }
-    />
-    <Route
-      path="register"
-      element={
-        <PageWrapper animation={slideRight}>
-          <Register />
-        </PageWrapper>
-      }
-    />
-    <Route
-      path="password-reset"
-      element={
-        <PageWrapper animation={slideRight}>
-          <PasswordReset />
-        </PageWrapper>
-      }
-    />
+    <Route path="language" element={<PageWrapper animation={scaleFade}><LanguageSelector /></PageWrapper>} />
+    <Route path="login" element={<PageWrapper animation={slideRight}><Login /></PageWrapper>} />
+    <Route path="register" element={<PageWrapper animation={slideRight}><Register /></PageWrapper>} />
+    <Route path="password-reset" element={<PageWrapper animation={slideRight}><PasswordReset /></PageWrapper>} />
   </>
 );
 
-export default PublicRoutes;
+export default RelationshipPublicRoutes;
