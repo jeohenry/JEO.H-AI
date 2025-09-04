9eda32ba6e0a0062d502917ade8c40094b2271e9
jeohenry <efuoyoma@gmail.com>
2025-09-04 17:33:19 +0100

    Update RelationshipPublicRoutes.jsx

--- src/routes/RelationshipPublicRoutes.jsx
+++ src/routes/RelationshipPublicRoutes.jsx
@@ -1,19 +1,66 @@
-//src/routes/RelationshipPublicRoutes.jsx
+// src/routes/RelationshipPublicRoutes.jsx
 
 import React from "react";
 import { Route } from "react-router-dom";
 import PageWrapper from "@/components/PageWrapper";
 import { slideRight, scaleFade } from "@/config/animations";
 
+// Auth pages
 import Login from "@/pages/Auth/Login";
 import Register from "@/pages/Auth/Register";
+import PasswordReset from "@/pages/Auth/PasswordReset";
+import LanguageSelector from "@/pages/Auth/LanguageSelector";
+
+// Posts
 import TranslatedPost from "@/pages/post/TranslatedPost";
 
 const PublicRoutes = () => (
   <>
-    <Route path="login" element={<PageWrapper animation={slideRight}><Login /></PageWrapper>} />
-    <Route path="register" element={<PageWrapper animation={slideRight}><Register /></PageWrapper>} />
-    <Route path="/post/:id/translated" element={<PageWrapper animation={scaleFade}><TranslatedPost /></PageWrapper>} />
+    {/* Language selection entry point */}
+    <Route
+      path="language"
+      element={
+        <PageWrapper animation={scaleFade}>
+          <LanguageSelector />
+        </PageWrapper>
+      }
+    />
+
+    {/* Authentication routes */}
+    <Route
+      path="login"
+      element={
+        <PageWrapper animation={slideRight}>
+          <Login />
+        </PageWrapper>
+      }
+    />
+    <Route
+      path="register"
+      element={
+        <PageWrapper animation={slideRight}>
+          <Register />
+        </PageWrapper>
+      }
+    />
+    <Route
+      path="password-reset"
+      element={
+        <PageWrapper animation={slideRight}>
+          <PasswordReset />
+        </PageWrapper>
+      }
+    />
+
+    {/* Translated post */}
+    <Route
+      path="post/:id/translated"
+      element={
+        <PageWrapper animation={scaleFade}>
+          <TranslatedPost />
+        </PageWrapper>
+      }
+    />
   </>
 );
 
