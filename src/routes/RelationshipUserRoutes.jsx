1623c0c10c4e3b82c1153425543fcad1216e8578
jeohenry <efuoyoma@gmail.com>
2025-09-04 16:59:10 +0100

    Update RelationshipUserRoutes.jsx

--- src/routes/RelationshipUserRoutes.jsx
+++ src/routes/RelationshipUserRoutes.jsx
@@ -1,10 +1,13 @@
-//src/routes/RelationshipUserRoutes.jsx
+// src/routes/RelationshipUserRoutes.jsx
 
 import React from "react";
 import { Route } from "react-router-dom";
 import PageWrapper from "@/components/PageWrapper";
 import {
-  slideUp, slideRight, scaleFade, pageFade
+  slideUp,
+  slideRight,
+  scaleFade,
+  pageFade,
 } from "@/config/animations";
 
 import Profile from "@/pages/relationship/Profile";
@@ -17,19 +20,107 @@ import LiveVideo from "@/pages/relationship/LiveVideoChat";
 import Notifications from "@/pages/Notifications";
 import UploadPicture from "@/pages/relationship/Upload";
 import LiveTranslatorTabs from "@/modules/LiveTranslatorTabs";
+import TranslatedFeed from "@/pages/relationship/TranslatedFeed";
+import RelationshipTools from "@/pages/relationship/RelationshipTools";
 
 const UserRoutes = () => (
   <>
-    <Route path="profile" element={<PageWrapper animation={pageFade}><Profile /></PageWrapper>} />
-    <Route path="feed" element={<PageWrapper animation={slideUp}><Feed /></PageWrapper>} />
-    <Route path="chatroom" element={<PageWrapper animation={slideRight}><ChatRoom /></PageWrapper>} />
-    <Route path="matchmaker" element={<PageWrapper animation={scaleFade}><MatchMaker /></PageWrapper>} />
-    <Route path="advice" element={<PageWrapper animation={scaleFade}><Advice /></PageWrapper>} />
-    <Route path="group-video-call" element={<PageWrapper animation={slideUp}><GroupVideoCall /></PageWrapper>} />
-    <Route path="live-video-chat" element={<PageWrapper animation={slideRight}><LiveVideo /></PageWrapper>} />
-    <Route path="notifications" element={<PageWrapper animation={pageFade}><Notifications /></PageWrapper>} />
-    <Route path="upload" element={<PageWrapper animation={slideRight}><UploadPicture /></PageWrapper>} />
-    <Route path="translate" element={<PageWrapper animation={scaleFade}><LiveTranslatorTabs /></PageWrapper>} />
+    <Route
+      path="profile"
+      element={
+        <PageWrapper animation={pageFade}>
+          <Profile />
+        </PageWrapper>
+      }
+    />
+    <Route
+      path="feed"
+      element={
+        <PageWrapper animation={slideUp}>
+          <Feed />
+        </PageWrapper>
+      }
+    />
+    <Route
+      path="translated-feed"
+      element={
+        <PageWrapper animation={slideUp}>
+          <TranslatedFeed />
+        </PageWrapper>
+      }
+    />
+    <Route
+      path="chatroom"
+      element={
+        <PageWrapper animation={slideRight}>
+          <ChatRoom />
+        </PageWrapper>
+      }
+    />
+    <Route
+      path="matchmaker"
+      element={
+        <PageWrapper animation={scaleFade}>
+          <MatchMaker />
+        </PageWrapper>
+      }
+    />
+    <Route
+      path="advice"
+      element={
+        <PageWrapper animation={scaleFade}>
+          <Advice />
+        </PageWrapper>
+      }
+    />
+    <Route
+      path="tools"
+      element={
+        <PageWrapper animation={scaleFade}>
+          <RelationshipTools />
+        </PageWrapper>
+      }
+    />
+    <Route
+      path="group-video-call"
+      element={
+        <PageWrapper animation={slideUp}>
+          <GroupVideoCall />
+        </PageWrapper>
+      }
+    />
+    <Route
+      path="live-video-chat"
+      element={
+        <PageWrapper animation={slideRight}>
+          <LiveVideo />
+        </PageWrapper>
+      }
+    />
+    <Route
+      path="notifications"
+      element={
+        <PageWrapper animation={pageFade}>
+          <Notifications />
+        </PageWrapper>
+      }
+    />
+    <Route
+      path="upload"
+      element={
+        <PageWrapper animation={slideRight}>
+          <UploadPicture />
+        </PageWrapper>
+      }
+    />
+    <Route
+      path="translate"
+      element={
+        <PageWrapper animation={scaleFade}>
+          <LiveTranslatorTabs />
+        </PageWrapper>
+      }
+    />
   </>
 );
 
