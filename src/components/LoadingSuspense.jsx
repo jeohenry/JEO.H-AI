932e190691f7922b33cfe49019f6552aade8650c
jeohenry <efuoyoma@gmail.com>
2025-09-05 19:38:56 +0100

    Update LoadingSuspense.jsx

--- src/components/LoadingSuspense.jsx
+++ src/components/LoadingSuspense.jsx
@@ -2,15 +2,14 @@
 import React, { useEffect } from "react";
 import { Suspense } from "react";
 import { useLoading } from "@/context/LoadingContext";
-import Loading from "@/components/Loading";
 
 const LoadingSuspense = ({ children }) => {
-  const { startLoading, stopLoading } = useLoading();
+  const { startSuspense, stopSuspense } = useLoading();
 
   useEffect(() => {
-    startLoading(); // show loader when Suspense mounts
-    return () => stopLoading(); // hide loader when ready
-  }, [startLoading, stopLoading]);
+    startSuspense();
+    return () => stopSuspense();
+  }, [startSuspense, stopSuspense]);
 
   return <Suspense fallback={<div />}>{children}</Suspense>;
 };
