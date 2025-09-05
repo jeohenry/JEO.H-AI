a757228c18c27104140b93f72770a173cb5284cb
jeohenry <efuoyoma@gmail.com>
2025-09-05 17:21:54 +0100

    Update TranslatedFeed.jsx

--- src/pages/relationship/TranslatedFeed.jsx
+++ src/pages/relationship/TranslatedFeed.jsx
@@ -1,7 +1,7 @@
 //src/pages/relationship/TranslatedFeed.jsx
 
 import React, { useEffect, useState } from "react";
-import axios from "../../utils/API";
+import axios from "../api";
 import { useTranslation } from "react-i18next";
 
 const TranslatedFeed = () => {
