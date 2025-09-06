899d2a85b2c6f15bdcc10342420a6343e0d5f1d5
jeohenry <efuoyoma@gmail.com>
2025-09-06 06:50:59 +0100

    Update i18n.js

--- src/i18n.js
+++ src/i18n.js
@@ -4,36 +4,16 @@ import { initReactI18next } from "react-i18next";
 import HttpBackend from "i18next-http-backend";
 import LanguageDetector from "i18next-browser-languagedetector";
 
-// Local static translations
-import translationEN from "./locales/en.json";
-import translationFR from "./locales/fr.json";
-import translationES from "./locales/es.json";
-import translationYO from "./locales/yo.json";
-import translationIG from "./locales/ig.json";
-import translationHA from "./locales/ha.json";
-import translationZH from "./locales/zh.json";
-import translationURH from "./locales/urh.json";
-
 i18n
-  .use(HttpBackend) // load from public/locales if not statically provided
+  .use(HttpBackend) // fetches translations from /public/locales/{{lng}}.json
   .use(LanguageDetector)
   .use(initReactI18next)
   .init({
     fallbackLng: "en",
     debug: false,
     supportedLngs: ["en", "fr", "es", "yo", "ig", "ha", "zh", "urh"],
-    resources: {
-      en: { translation: translationEN },
-      fr: { translation: translationFR },
-      es: { translation: translationES },
-      yo: { translation: translationYO },
-      ig: { translation: translationIG },
-      ha: { translation: translationHA },
-      zh: { translation: translationZH },
-      urh: { translation: translationURH },
-    },
     backend: {
-      loadPath: "/locales/{{lng}}.json",
+      loadPath: "/locales/{{lng}}.json", // served from public/locales
     },
     detection: {
       order: ["localStorage", "navigator", "htmlTag", "querystring", "cookie"],
@@ -48,10 +28,3 @@ i18n
   });
 
 export default i18n;
-
-
-
-
-
-
-
